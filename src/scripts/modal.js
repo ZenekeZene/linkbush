420d9eeb31c5089c3c999b27121876e86c37c676
Zeneke <hectorvillarm@gmail.com>
2022-11-19 18:40:31 +0100

    Things.

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,7 +1,7 @@
 function handleModal () {
   const modal = document.getElementById('modal')
   const triggers = document.querySelectorAll('.modal-trigger')
-  console.log(modal, triggers)
+
   triggers.forEach(trigger => {
     trigger.addEventListener('click', (event) => {
       event.stopPropagation()
