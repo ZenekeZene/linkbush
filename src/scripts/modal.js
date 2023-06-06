b7274f0113f1301399dc12fc103bf1e41f199468
Zeneke <hectorvillarm@gmail.com>
2023-06-07 01:07:50 +0200

    overflow body and modal

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,14 +1,17 @@
 function handleModal () {
   const modal = document.getElementById('modal')
   const triggers = document.querySelectorAll('.modal-trigger')
+  const body = document.body
 
   triggers.forEach(trigger => {
     trigger.addEventListener('click', (event) => {
       event.stopPropagation()
       if (modal.classList.contains('visible')) {
         modal.classList.remove('visible')
+        body.classList.remove('modal-open')
       } else {
         modal.classList.add('visible')
+        body.classList.add('modal-open')
       }
     })
   })
